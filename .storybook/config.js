a712fa022d62ecc64416c9285511842791486155
Graham Hukill <ghukill@gmail.com>
2019-08-21 15:26:28 -0400

    JTable component

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from '@storybook/vue';
 // automatically import all files ending in *.stories.js
 const req = require.context(
-  '../components/JTableRow',
+  '../components',
   true,
   /\.stories\.js$/
 );
